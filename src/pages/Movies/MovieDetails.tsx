9f4a0320979636c5e93315baee08010ca7a78fb3
saminravi11 <saminravi.github@gmail.com>
2024-07-08 15:14:27 +0600

    user login register done

--- src/pages/Movies/MovieDetails.tsx
+++ src/pages/Movies/MovieDetails.tsx
@@ -9,7 +9,7 @@ import { useParams } from "react-router-dom";
 export default function MovieDetails() {
   const { id: slug } = useParams();
   // console.log(slug);
-  const { data, isLoading } = useGetMovieDetailsAndReviewsQuery(slug as string);
+  const { data, isLoading } = useGetMovieDetailsAndReviewsQuery(slug);
 
   if (isLoading)
     return (
